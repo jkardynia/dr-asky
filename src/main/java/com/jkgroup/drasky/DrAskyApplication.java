7ad13bf3c204506673568f58f565926bf4d05584
Jaroslaw Kardynia <jjarekk@gmail.com>
2018-12-09 13:34:03 +0100

    Initializing app

---
+++ src/main/java/com/jkgroup/drasky/DrAskyApplication.java
@@ -0,0 +1,12 @@
+package com.jkgroup.drasky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrAskyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrAskyApplication.class, args);
+	}
+}
